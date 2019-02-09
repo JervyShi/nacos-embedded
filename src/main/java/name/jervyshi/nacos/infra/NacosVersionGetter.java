e548b4701cf11b9af8c6d97aa1be601e0253926b
JervyShi <jervyshi@gmail.com>
2019-02-10 02:06:11 +0800

    add travis ci support

--- src/main/java/name/jervyshi/nacos/infra/NacosVersionGetter.java
+++ src/main/java/name/jervyshi/nacos/infra/NacosVersionGetter.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package name.jervyshi.nacos.infra;
 
 import java.io.BufferedReader;
@@ -26,7 +42,7 @@ public class NacosVersionGetter {
 
     /** logger */
     private static final Logger logger           = LoggerFactory
-        .getLogger(NacosVersionGetter.class);
+                                                     .getLogger(NacosVersionGetter.class);
 
     /**
      * Gets latest version.
